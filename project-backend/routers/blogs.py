4379419f860e2d8fb5b394eaf0b67d7daf5fc1a7
tboy54321 <akinniranoluwatosin22@gmail.com>
2024-08-05 23:50:37 +0100

    updated blogs api endpoints

--- project-backend/routers/blogs.py
+++ project-backend/routers/blogs.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI, APIRouter, Depends
+from fastapi import FastAPI, APIRouter, Depends, HTTPException, status
 import models, schemas, oauth2
 from sqlalchemy.orm import Session
 from database import get_db
@@ -9,11 +9,22 @@ router = APIRouter(tags=["Blogs Page"])
 
 @router.get("/posts")
 def get_all_blogs(db: Session = Depends(get_db), current_user: int = Depends(oauth2.get_current_user)):
-    all_blogs = db.query(models.blogs).all()
+    all_blogs = db.query(models.BlogPost).all()
+    print(current_user)
     return all_blogs
 
 
-@router.get("/posts/:{id}")
-def get_one_post(db: Session = Depends(get_db), current_user: int = Depends(oauth2.get_current_user)):
-    blog = db.query(models.blogs.id == id).first()
+@router.get("/posts/{id}")
+def get_one_post(id: int, db: Session = Depends(get_db), current_user: int = Depends(oauth2.get_current_user)):
+    blog = db.query(models.BlogPost).filter(models.BlogPost.id == id).first()
+    
+    if not blog:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"Blog with id {id} does not exist")
+    
     return blog
+
+
+# @router.post("/post"):
+# def new_post(post: schemas, db: Session = Depends(get_db), current_user: int = Depends(oauth2.get_current_user)):
+#     blog = db.query
+#     db.add
